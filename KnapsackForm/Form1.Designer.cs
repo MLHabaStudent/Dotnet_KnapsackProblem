32808943645d91e245fbb14334d5863e9696893f
Michał Haba <mhaba.0005@gmail.com>
2022-03-15 23:03:28 +0100

    Created form

---
+++ KnapsackForm/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace KnapsackForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtKnapsack = new System.Windows.Forms.TextBox();
+            this.txtItems = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCount = new System.Windows.Forms.TextBox();
+            this.txtCap = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSeed = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtKnapsack
+            // 
+            this.txtKnapsack.BackColor = System.Drawing.SystemColors.Window;
+            this.txtKnapsack.Location = new System.Drawing.Point(9, 197);
+            this.txtKnapsack.Margin = new System.Windows.Forms.Padding(2);
+            this.txtKnapsack.Multiline = true;
+            this.txtKnapsack.Name = "txtKnapsack";
+            this.txtKnapsack.ReadOnly = true;
+            this.txtKnapsack.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtKnapsack.Size = new System.Drawing.Size(223, 240);
+            this.txtKnapsack.TabIndex = 1;
+            // 
+            // txtItems
+            // 
+            this.txtItems.BackColor = System.Drawing.SystemColors.Window;
+            this.txtItems.Location = new System.Drawing.Point(251, 23);
+            this.txtItems.Margin = new System.Windows.Forms.Padding(2);
+            this.txtItems.Multiline = true;
+            this.txtItems.Name = "txtItems";
+            this.txtItems.ReadOnly = true;
+            this.txtItems.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtItems.Size = new System.Drawing.Size(223, 414);
+            this.txtItems.TabIndex = 2;
+            this.txtItems.Text = "Items";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Item count";
+            // 
+            // txtCount
+            // 
+            this.txtCount.Location = new System.Drawing.Point(9, 23);
+            this.txtCount.Margin = new System.Windows.Forms.Padding(2);
+            this.txtCount.Name = "txtCount";
+            this.txtCount.Size = new System.Drawing.Size(221, 20);
+            this.txtCount.TabIndex = 4;
+            // 
+            // txtCap
+            // 
+            this.txtCap.Location = new System.Drawing.Point(9, 60);
+            this.txtCap.Margin = new System.Windows.Forms.Padding(2);
+            this.txtCap.Name = "txtCap";
+            this.txtCap.Size = new System.Drawing.Size(221, 20);
+            this.txtCap.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 45);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Knapsack size";
+            // 
+            // txtSeed
+            // 
+            this.txtSeed.Location = new System.Drawing.Point(9, 97);
+            this.txtSeed.Margin = new System.Windows.Forms.Padding(2);
+            this.txtSeed.Name = "txtSeed";
+            this.txtSeed.Size = new System.Drawing.Size(221, 20);
+            this.txtSeed.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 82);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Seed";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 134);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(223, 35);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Solve";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 182);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Knapsack";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 446);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtSeed);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCap);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtItems);
+            this.Controls.Add(this.txtKnapsack);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtKnapsack;
+        private System.Windows.Forms.TextBox txtItems;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCount;
+        private System.Windows.Forms.TextBox txtCap;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSeed;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
